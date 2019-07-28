b2d383ff8c228f4fe230d9aa92a7dbc0d6d60256
eltneg <yorewole@gmail.com>
2019-07-28 16:09:52 +0100

    Add UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextArea, Button, Input, Form } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import './assets/style.css';
 
+const initialState = {
+  tokenAddress: '',
+  addresses: '',
+  result: ''
+};
 function App() {
+  const [state, setState] = useState(initialState);
+  const { tokenAddress, addresses, result } = state;
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setState(prevState => {
+      return { ...prevState, [name]: value };
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        Balanchecker
-      </header>
+      <header className="App-header">Balanchecker</header>
       <main>
-        Main App
+        <Form autoComplete="off">
+        <Input onChange={handleChange} name="tokenAddress" value={tokenAddress} placeholder="Token address" />
+        <TextArea onChange={handleChange} name="addresses" value={addresses} placeholder="Paste your ethereum addresses here" />
+        <Button>Check balance</Button>
+        <TextArea value={result} onChange={handleChange} placeholder="Results" />
+        <Button>Download CSV</Button>
+        </Form>
+        
       </main>
     </div>
   );
