3c07615d33b1e992f9911700909f8aa4491cef75
eltneg <yorewole@gmail.com>
2019-07-28 17:53:27 +0100

    Implement download as CSV

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 import { TextArea, Button, Input, Form } from 'semantic-ui-react';
+import { CSVLink } from 'react-csv';
 import { getBalances } from './utils/funcs';
 import 'semantic-ui-css/semantic.min.css';
 import './assets/style.css';
 
-
 const initialState = {
   tokenAddress: '',
   addresses: '',
-  result: {}
+  result: {},
+  csvData: []
 };
 function App() {
   const [state, setState] = useState(initialState);
-  const { tokenAddress, addresses, result } = state;
+  const { tokenAddress, addresses, result, csvData } = state;
   const handleChange = e => {
     const { name, value } = e.target;
     setState(prevState => {
@@ -24,15 +25,42 @@ function App() {
       <header className="App-header">Balanchecker</header>
       <main>
         <Form autoComplete="off">
-        <Input autoComplete="off" onChange={handleChange} name="tokenAddress" value={tokenAddress} placeholder="Token address" />
-        <TextArea onChange={handleChange} name="addresses" value={addresses} placeholder="Paste your ethereum addresses here" />
-        <Button onClick={() => getBalances(tokenAddress, addresses).then((result) => {
-          setState(prevState => ({...prevState, result}))
-        })}>Check balance</Button>
-        <TextArea value={JSON.stringify(result)} onChange={handleChange} placeholder="Results" />
-        <Button>Download CSV</Button>
+          <Input
+            autoComplete="off"
+            onChange={handleChange}
+            name="tokenAddress"
+            value={tokenAddress}
+            placeholder="Token address"
+          />
+          <TextArea
+            onChange={handleChange}
+            name="addresses"
+            value={addresses}
+            placeholder="Paste your ethereum addresses here"
+          />
+          <Button
+            onClick={() =>
+              getBalances(tokenAddress, addresses).then(
+                ({ result, csvData }) => {
+                  setState(prevState => ({ ...prevState, result, csvData }));
+                }
+              )
+            }
+          >
+            Check balance
+          </Button>
+          <TextArea
+            value={JSON.stringify(result)}
+            onChange={handleChange}
+            placeholder="Results"
+          />
+
+          {csvData.length ? (
+            <CSVLink filename="data.csv" data={csvData}>
+              Download CSV
+            </CSVLink>
+          ) : null}
         </Form>
-        
       </main>
     </div>
   );
--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,27 +1,26 @@
-import web3, {tokenContract} from './setup';
+import web3, { tokenContract } from './setup';
 
-
-export const getBalances = async(tokenAddress, addresses) => {
-    let getBalance
-    const addressList = [...(new Set(addresses.split(" ")))]
-    if(tokenAddress){
-        getBalance = (addr) => tokenContract(tokenAddress).methods.balanceOf(addr).call()
-        
-    }else{
-        getBalance = web3.eth.getBalance;
-    }
+export const getBalances = async (tokenAddress, addresses) => {
+  let getBalance;
+  const addressList = [...new Set(addresses.split(' '))];
+  if (tokenAddress) {
+    getBalance = addr =>
+      tokenContract(tokenAddress)
+        .methods.balanceOf(addr)
+        .call();
+  } else {
+    getBalance = web3.eth.getBalance;
+  }
   const addressesPromise = addressList.map(address => {
-    if(web3.utils.isAddress(address)){
-        return getBalance(address);
+    if (web3.utils.isAddress(address)) {
+      return getBalance(address);
     }
     return 'invalid address';
   });
-  const balances = await Promise.all(addressesPromise)
-  return addressList.reduce((result, address, index) => {
-      result[address] = balances[index]
-      return result
-  }, {})
+  const balances = await Promise.all(addressesPromise);
+  return addressList.reduce((accummulator, address, index) => {
+    accummulator.result[address] = balances[index];
+    accummulator.csvData.push([address, balances[index]])
+    return accummulator;
+  }, {result: {}, csvData: [["Address", "Balance"]]});
 };
-
-
-
