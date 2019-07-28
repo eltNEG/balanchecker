c62a85ec5d732b4a0082514eebf4e620f98e8952
eltneg <yorewole@gmail.com>
2019-07-28 15:54:16 +0100

    Initial commit - setup project structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import './assets/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Balanchecker
+      </header>
+      <main>
+        Main App
+      </main>
+    </div>
+  );
+}
+
+export default App;
