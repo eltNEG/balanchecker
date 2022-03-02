acbdc9062226f949a6bbc1325e92c0b171069d68
Peter Conerly <pconerly@gmail.com>
2022-03-01 22:17:15 -0800

    Check for multiple invalid addresses and provide results

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -3,25 +3,39 @@ import ethers, { addrBalContract } from './setup';
 export const getBalances = async (tokenAddress, addresses) => {
   const addressList = generateAddresses([...new Set(addresses.split(/\s+/))]);
 
+  const invalidAddresses = []
+  const validAddresses = []
   for(const address of addressList){
-    if(!ethers.utils.isAddress(address)){
-      alert(`Invalid Address: ${address}`)
-      return {result: {}, csvData: [["Address", "Balance"]]}
+    if(ethers.utils.isAddress(address)){
+      validAddresses.push(address)
+    } else {
+      invalidAddresses.push(address)
     }
   }
   try {
     let balances
-  if(tokenAddress === '0x0000000000000000000000000000000000000000' || !tokenAddress){
-    balances = await addrBalContract().getBalances(addressList)
-  }else{
-    balances = await addrBalContract().getTokenBalances(tokenAddress, addressList)
-  }
-  console.log(balances)
-  return addressList.reduce((accummulator, address, index) => {
-    accummulator.result[address] = (balances[index]).toString();
-    accummulator.csvData.push([address, (balances[index]).toString()])
-    return accummulator;
-  }, {result: {}, csvData: [["Address", "Balance"]]});
+    if(tokenAddress === '0x0000000000000000000000000000000000000000' || !tokenAddress){
+      balances = await addrBalContract().getBalances(validAddresses)
+    }else{
+      balances = await addrBalContract().getTokenBalances(tokenAddress, validAddresses)
+    }
+    console.log(balances)
+    let results = validAddresses.reduce((accummulator, address, index) => {
+      accummulator.result[address] = (balances[index]).toString();
+      accummulator.csvData.push([address, (balances[index]).toString()])
+      return accummulator;
+    }, {result: {}, csvData: [["Address", "Balance"]]});
+    if (invalidAddresses.length > 0) {
+      alert(`Invalid Addresses:\n ${invalidAddresses.join('\n')}`)
+    }
+
+    // include invalid addresses at the bottom
+    results = invalidAddresses.reduce((accummulator, address) => {
+      accummulator.result[address] = 'invalid address';
+      accummulator.csvData.push([address, 'invalid address']);
+      return accummulator;
+    }, results);
+    return results;
   } catch (error) {
     console.log(error)
     return {result: {}, csvData: [["Address", "Balance"]]} 
